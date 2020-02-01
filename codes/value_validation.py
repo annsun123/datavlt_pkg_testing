9f43cb92463430e1cfab8cafe94e56de3b8ff8ea
annsun123 <42828424+annsun123@users.noreply.github.com>
2020-02-01 12:14:33 +0800

    Update value_validation.py

--- codes/value_validation.py
+++ codes/value_validation.py
@@ -1,31 +1,44 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Nov 29 11:20:10 2019
-
-@author: anyan.sun
-"""
-
 import json 
-import pandas as pd 
 from codes.class_logging import logging_func
-
-
-validation=logging_func('value_validation_log',filepath='')
-validationlogger=validation.myLogger()
+validation = logging_func('value_validation_log',filepath='')
+validationlogger = validation.myLogger()
 
 def problem_values(df_final):
-    
-    df_final['Date']=df_final['Date'].astype(str)
-    problematic_values=df_final[(df_final['qty(MC)']<0) | (df_final[['sku','series','customer_name','Date','system_date']].isnull().any(axis=1))][['customer_name','sku','Date','qty(MC)']]
-    
-    problematic_values=json.loads(problematic_values.astype(object).where(problematic_values.notnull(),None).to_json(orient='records'))
-    if len(problematic_values)>0:
-        validationlogger.error('existing nan or negative quantity values')
-        
-        #sending the problematic_value through the email
+    if 'Distribution Center' in df_final.columns:
+        date_col = 'Invoice Date'
+        cname_col = 'Distribution Center'
+        sku_col = 'SKU'
+        series_col = 'Series'
     else:
+        date_col = 'Date'
+        cname_col = 'customer_name'
+        sku_col = 'sku'
+        series_col = 'series'
 
+    df_final[date_col] = df_final[date_col].astype(str)
+    problematic_values = df_final[
+        (df_final['qty(MC)'] < 0) | (df_final[[sku_col, series_col,
+                                               cname_col, 
+                                               date_col,
+                                               'system_date']].isnull().any(axis=1))
+    ][[cname_col, sku_col, date_col, 'qty(MC)']]
+    
+    problematic_values = json.loads(
+        problematic_values.astype(object).where(problematic_values.notnull(),None).to_json(orient='records')
+    )
+    if len(problematic_values) > 0:
+        validationlogger.error('existing nan or negative quantity values')
+      
+#sending the problematic_value through the email
+    else:
         validationlogger.info('all values are valid')
-    validationlogger.error(problematic_values)
-    validationlogger.error('the index {} contains null'.format(list(df_final[df_final[['sku','series','customer_name','Date','system_date']].isnull().any(axis=1)].index)))
+        validationlogger.error(problematic_values)
+        validationlogger.error(
+            'the index {} contains null'.format(list(
+                df_final[df_final[[sku_col,
+                                   series_col,
+                                   cname_col, date_col,
+                                   'system_date']].isnull().any(axis=1)].index)
+                                               )
+        )
     return problematic_values
