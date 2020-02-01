4f4af55c363e1b6e332d9555508f77182b58bdd7
annsun123 <42828424+annsun123@users.noreply.github.com>
2020-02-01 12:20:35 +0800

    Update class_logging.py

--- codes/class_logging.py
+++ codes/class_logging.py
@@ -10,17 +10,16 @@ import datetime
 
 
 class logging_func:
-    def __init__(self,logger_name,filepath):
-        self.logger_name=logger_name
-        self.filepath=filepath
+    def __init__(self, logger_name, filepath):
+        self.logger_name = logger_name
+        self.filepath = filepath
         
     def myLogger(self):
 
-        logger=logging.getLogger(self.logger_name)
+        logger = logging.getLogger(self.logger_name)
         logger.setLevel(logging.DEBUG)
-
-        now = datetime.datetime.now()
-        handler=logging.FileHandler(self.filepath + 'log/'+ self.logger_name +format(datetime.datetime.now(),"%m_%d_%Y")+'.log')
+        
+        handler = logging.FileHandler(self.filepath + 'log/' + self.logger_name + format(datetime.datetime.now(), "%m_%d_%Y") + '.log')
 
         formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
         handler.setFormatter(formatter)
