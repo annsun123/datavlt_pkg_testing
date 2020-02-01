f91db30f04a0649ec583759ea24e4c5eda866360
annsun123 <42828424+annsun123@users.noreply.github.com>
2020-02-01 18:38:15 +0800

    Update value_validation.py

--- codes/value_validation.py
+++ codes/value_validation.py
@@ -18,28 +18,31 @@ def problem_values(df_final):
 
     df_final[date_col] = df_final[date_col].astype(str)
     problematic_values = df_final[
-        (df_final['qty(MC)'] < 0) | (df_final[[sku_col, series_col,
-                                               cname_col, 
+        (df_final['qty(MC)'] < 0) | (df_final[[sku_col,
+                                               series_col,
+                                               cname_col,
                                                date_col,
                                                'system_date']].isnull().any(axis=1))
-    ][[cname_col, sku_col, date_col, 'qty(MC)']]
-    
+    ][[cname_col,
+       sku_col,
+       date_col,
+       'qty(MC)']]
+
     problematic_values = json.loads(
         problematic_values.astype(object).where(problematic_values.notnull(),None).to_json(orient='records')
     )
     if len(problematic_values) > 0:
         validationlogger.error('existing nan or negative quantity values')
-      
+
 #sending the problematic_value through the email
     else:
         validationlogger.info('all values are valid')
         validationlogger.error(problematic_values)
         validationlogger.error(
-            'the index {} contains null'.format(list(
-                df_final[df_final[[sku_col,
-                                   series_col,
-                                   cname_col, date_col,
-                                   'system_date']].isnull().any(axis=1)].index)
-                                               )
+            'the index {} contains null'.format(list(df_final[df_final[[sku_col,
+                                                                        series_col,
+                                                                        cname_col, 
+                                                                        date_col,
+                                                                        'system_date']].isnull().any(axis=1)].index))
         )
     return problematic_values
