8d90da9c5caad9e976f946e56fcd5eb435daab60
annsun123 <42828424+annsun123@users.noreply.github.com>
2020-02-01 18:30:49 +0800

    Update class_sending_email.py

--- codes/class_sending_email.py
+++ codes/class_sending_email.py
@@ -15,20 +15,20 @@ import os
 
 def sending_emails(from_address, user_name, password, receiving_address):
     EMAIL_HOST = 'email-smtp.us-east-1.amazonaws.com'
-    EMAIL_HOST_USER = user_name 
-    EMAIL_HOST_PASSWORD =password  
+    EMAIL_HOST_USER = user_name
+    EMAIL_HOST_PASSWORD =password
     EMAIL_PORT = 587
-      
+
     msg = MIMEMultipart('alternative')
     msg['Subject'] = "Error_logs "
     msg['From'] = from_address
     msg['To'] = ",".join(receiving_address)
-      
+
 # First Half
     email_content = 'This email contains log files of ' + format(datetime.datetime.now(), "%m_%d_%Y")
     mime_text = MIMEText(email_content, 'html')
     msg.attach(mime_text)
-      
+
 # Second Half
     for file in os.listdir('log'):
         attachment = open('log/'+file, 'rb')
@@ -39,7 +39,7 @@ def sending_emails(from_address, user_name, password, receiving_address):
         encoders.encode_base64(part)
         part.add_header('Content-Disposition', 'attachment', filename=file_name)
         msg.attach(part)
-        
+
 # Sending the emial
     s = smtplib.SMTP(EMAIL_HOST, EMAIL_PORT)
     s.starttls()
