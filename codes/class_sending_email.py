4a6c672ac95dd98027107f32a81c0c31b0ddf85b
annsun123 <42828424+annsun123@users.noreply.github.com>
2020-02-01 15:39:49 +0800

    Update class_sending_email.py

--- codes/class_sending_email.py
+++ codes/class_sending_email.py
@@ -10,45 +10,39 @@ from email.mime.text import MIMEText
 from email import encoders
 from email.mime.base import MIMEBase
 import datetime
-import os 
+import os
 
 
-def sending_emails(from_address,user_name,password,receiving_address):
+def sending_emails(from_address, user_name, password, receiving_address):
     EMAIL_HOST = 'email-smtp.us-east-1.amazonaws.com'
-    EMAIL_HOST_USER = user_name#"AKIAWHILWELFJY4H7MG2" # Replace with your SMTP username
-    EMAIL_HOST_PASSWORD =password #"BGjsPmXs4QH3yzaeX9dmheu0KdTnWKKOnmx5D4+NjuSd" # Replace with your SMTP password
+    EMAIL_HOST_USER = user_name 
+    EMAIL_HOST_PASSWORD =password  
     EMAIL_PORT = 587
-    
-     
-    
+      
     msg = MIMEMultipart('alternative')
     msg['Subject'] = "Error_logs "
-    msg['From'] = from_address # "noreply@datavlt.com"
-    msg['To'] =",".join(receiving_address) # "anyan.sun@datavlt.com"
-    
-     
-    #### First Half######
-    email_content='This email contains log files of '+format(datetime.datetime.now(),"%m_%d_%Y")
+    msg['From'] = from_address
+    msg['To'] = ",".join(receiving_address)
+      
+# First Half
+    email_content = 'This email contains log files of ' + format(datetime.datetime.now(), "%m_%d_%Y")
     mime_text = MIMEText(email_content, 'html')
     msg.attach(mime_text)
-    
-     
-    ######### Second Half ############
+      
+# Second Half
     for file in os.listdir('log'):
-        attachment=open('log/'+file,'rb')
-        file_name=file
+        attachment = open('log/'+file, 'rb')
+        file_name = file
         part = MIMEBase('application', "octet-stream")
-        part.set_payload(attachment.read())  #if wnat to sending the log fi
+# if wnat to sending the log fi   
+        part.set_payload(attachment.read())
         encoders.encode_base64(part)
         part.add_header('Content-Disposition', 'attachment', filename=file_name)
         msg.attach(part)
-    
-     
-    ############# Sending the emial ##########
+        
+# Sending the emial
     s = smtplib.SMTP(EMAIL_HOST, EMAIL_PORT)
     s.starttls()
     s.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
     s.sendmail(from_address, receiving_address, msg.as_string())
     s.quit()
-    
-    
