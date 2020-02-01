bbdbd82e2cac62f33ba1c20e43005d2a9e3524a9
annsun123 <42828424+annsun123@users.noreply.github.com>
2020-02-01 18:26:03 +0800

    Update class_rfm.py

--- codes/class_rfm.py
+++ codes/class_rfm.py
@@ -15,26 +15,34 @@ rfmlogger = rfm.myLogger()
 
 def createRFM_dataset(df):
     df_rfm = df.groupby(['customer_name',
-                         'customer_id', 'city' ]).agg(
-        {'invoice_date': 'max', 'Avg_order_gap': 'mean',
-         'qty_packs': 'count', 'amount': 'sum'}
-    ).reset_index()
+                         'customer_id', 
+                         'city' ]).agg({'invoice_date': 'max',
+                                        'Avg_order_gap': 'mean',
+                                        'qty_packs': 'count',
+                                        'amount': 'sum'}).reset_index()
     df_rfm['Recency'] = [pd.to_datetime(
-        df_rfm.loc[:, 'invoice_date']
-    ).max() - x for x in pd.to_datetime(df_rfm.loc[:, 'invoice_date'])]
+        df_rfm.loc[:, 'invoice_date']).max() - x for x in pd.to_datetime(df_rfm.loc[:, 'invoice_date']
+                                                                        )]
     df_rfm['Recency'] = df_rfm['Recency'].dt.days
     df_rfm.rename(columns={'amount': 'Monetary',
                            'qty_packs': 'Frequency'}, inplace=True)
     df_rfm.drop('invoice_date', axis=1, inplace=True)
     df_rfm['Avg_order_gap'] = round(df_rfm.Avg_order_gap)
-    
-    df_rfm['R_score'] = pd.qcut(df_rfm['Recency'], 4, labels=['4', '3', '2', '1'],
+
+    df_rfm['R_score'] = pd.qcut(df_rfm['Recency'],
+                                4,
+                                labels=['4', '3', '2', '1'],
                                 duplicates = 'drop').astype(int)
-    df_rfm['F_score'] = pd.cut(df_rfm['Frequency'], 4, labels=['1', '2', '3', '4'], 
+    df_rfm['F_score'] = pd.cut(df_rfm['Frequency'],
+                               4,
+                               labels=['1', '2', '3', '4'],
                                duplicates = 'drop').astype(int)
-    df_rfm['M_score'] = pd.qcut(df_rfm['Monetary'], 4, labels=['1', '2', '3', '4'], 
+    df_rfm['M_score'] = pd.qcut(df_rfm['Monetary'],
+                                4,
+                                labels=['1', '2', '3', '4'],
                                 duplicates = 'drop').astype(int)
-    df_rfm['RFM_score'] = df_rfm.apply(lambda x: (int(x.R_score)+ int(x.F_score) + int(x.M_score))/3, axis=1)
+    df_rfm['RFM_score'] = df_rfm.apply(lambda x: (int(x.R_score)+ int(x.F_score) + int(x.M_score))/3,
+                                       axis=1)
     return df_rfm
 
 
@@ -46,37 +54,35 @@ def clustering(df, str_cols, opt_clust):
         cluster = KMeans(init='k-means++', n_clusters=4, 
                          n_init=100, random_state=50)
         pred_clusters = cluster.fit(scaler.fit_transform(df[str_cols])).labels_
-       
+
     elif opt_clust.lower() == 'algo':
-        cluster = AgglomerativeClustering(n_clusters=4, 
-                                          affinity='euclidean', linkage='ward')
+        cluster = AgglomerativeClustering(n_clusters=4,
+                                          affinity='euclidean',
+                                          linkage='ward')
         pred_clusters = cluster.fit(scaler.fit_transform(df[str_cols])).labels_
     elif opt_clust.lower() == 'gmm':
-        cluster = GaussianMixture(n_components=4, random_state=42)
+        cluster = GaussianMixture(n_components=4,
+                                  random_state=42)
         pred_clusters = cluster.fit_predict(df[str_cols])
     else:
         rfmlogger.error('cluster option doesnt match')
-    rfmlogger.info('silhouette score', 
-                   silhouette_score(
-                       scaler.fit_transform(df[str_cols]),
-                       pred_clusters, random_state=42)
-                  )
+    rfmlogger.info('silhouette score',silhouette_score(scaler.fit_transform(df[str_cols]),
+                                                       pred_clusters,
+                                                       random_state=42))
     return ['cluster-' + str(x + 1) for x in pred_clusters]
 
 
 def defineCluster(df, str_cluter_def):
-    df_agg = df.groupby('cluster').agg({'R_score': 'mean', 
+    df_agg = df.groupby('cluster').agg({'R_score': 'mean',
                                         'F_score': 'mean',
-                                        'M_score': 'mean', 
+                                        'M_score': 'mean',
                                         'RFM_score': 'mean'})
-   
+
     df_agg.sort_values('RFM_score', ascending=False, inplace=True)
     dict_clust_def_overall = dict(zip(df_agg.index, str_cluter_def))
     df_agg.index = dict_clust_def_overall.values()
-   
+
     for i in range(4):
-        df.loc[df['cluster'] == list(
-            dict_clust_def_overall.keys()
-        )[i],'cluster'] = list(dict_clust_def_overall.values())[i]
+        df.loc[df['cluster'] == list(dict_clust_def_overall.keys())[i],'cluster'] = list(dict_clust_def_overall.values())[i]
 
     return df
