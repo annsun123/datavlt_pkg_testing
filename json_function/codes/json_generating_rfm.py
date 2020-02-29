c6f353944908759960ded6129f47b167bc526dca
annsun123 <42828424+annsun123@users.noreply.github.com>
2020-02-29 21:55:16 +0800

    Create json_generating_rfm.py

---
+++ json_function/codes/json_generating_rfm.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 27 14:50:03 2020
+
+@author: anyan.sun
+"""
+from json_function.codes.supporting_function import create_conn
+from codes.class_rfm import createRFM_dataset, clustering, defineCluster
+import pandas as pd 
+import datetime
+from psycopg2.extras import Json
+
+def creating_json_rfm():
+    conn=create_conn()
+    cur = conn.cursor()
+    
+    final_table=pd.read_sql_query("SELECT * from final_nonindo", conn)
+    
+    cur.close()
+    conn.close()
+    
+    
+    raw_ds = final_table.copy()
+    
+    if 'Unnamed: 0' in raw_ds:
+        raw_ds.drop('Unnamed: 0', axis=1, inplace=True)
+    
+       # raw_ds['amount']=raw_ds.apply(lambda x: x['qty_packs']*15000000 if x['sku']=='STARTER DECK' else  x['qty_packs']*30000000, axis=1)
+    raw_ds['amount']=raw_ds['amount']/1000000
+    raw_ds['invoice_date'] = pd.to_datetime(raw_ds['invoice_date'])
+    raw_ds['Avg_order_gap'] = (raw_ds['invoice_date'].max() - raw_ds['invoice_date']).dt.days
+        
+    
+    
+    str_cols = ['Frequency', 'Monetary', 'Avg_order_gap', 'Recency']
+    
+    df_rfm_overall = createRFM_dataset(raw_ds)
+    df_rfm_overall['cluster'] = clustering(df_rfm_overall, str_cols, "algo") # gives best 
+    #str_cluter_def = ['Star','Cash Cows', 'Question Marks', 'Dogs']
+    str_cluter_def = ['The Star Performer','High Spender', 'Loyal Spender', 'Low Spender']
+    df_rfm_overall = defineCluster(df_rfm_overall, str_cluter_def)
+       
+    df_rfm_output = final_table.merge(df_rfm_overall[['customer_id','cluster','Recency']],how='left', on='customer_id')
+    df_rfm_output['invoice_date'] = pd.to_datetime(df_rfm_output['invoice_date'])
+    
+    df_rfm_output['Avg_order_gap'] =  (df_rfm_output['invoice_date'].max() - df_rfm_output['invoice_date']).dt.days
+    df_rfm_overall=df_rfm_overall.rename(columns={'Avg_order_gap':'Time(Light)', 'Monetary':'Sales',\
+                         'Recency':'Time(Dark)', 'Frequency':'Purchase'})
+                         
+    df_rfm_city = df_rfm_overall.groupby(['cluster','city'])\
+    .agg({'Time(Light)': 'mean', 'Sales': 'sum', 'Time(Dark)': 'mean', \
+          'Purchase': 'mean'}).reset_index()
+    df_rfm_city=df_rfm_city.round({'Time(Dark)':0,'Time(Light)':0,'Purchase':0,'Sales':2})
+       
+                        
+    
+    df_rfm_output = df_rfm_output.groupby(['cluster','series_num', 'sku']).\
+    agg({'Recency': 'mean',  'Avg_order_gap': 'mean', \
+          'qty_packs': 'count', 'amount_million': 'sum'}).reset_index()
+    #df_rfm_output['invoice_date'] = df_rfm_output['invoice_date'].astype(str)
+    df_rfm_output=df_rfm_output.round({'Recency':0,'Avg_order_gap':0,'qty_packs':0,'amount':2})
+       
+    df_rfm_cn=df_rfm_overall[['cluster', 'customer_name', 'city', 'Time(Dark)', \
+                       'Sales', 'Time(Light)', 'Purchase']]
+    df_rfm_cn=df_rfm_cn.round({'Sales':2})
+    
+       
+    df_rfm_output=df_rfm_output.astype(object).where(df_rfm_output.notnull(),None)
+    
+    json_input_series = df_rfm_output.to_dict(orient='records')## for series
+    json_input_series.insert(0, {'graph_description': 'rfm filtering by series'})
+    json_input_city = df_rfm_city.to_dict(orient='records') ## for city
+    json_input_cn = df_rfm_cn.to_dict(orient='records') ## for customer 
+    
+    to_date = ''
+    from_date = ''
+    rfm_json_table = []
+    for (description, json_output) in [('rfm filtering by series', json_input_series),\
+                                ('rfm filtering by city', json_input_city),\
+                                ('rfm filtering by customer name', json_input_cn)]:
+     
+        rfm_json_table.append ([description, 'rfm', 'NA',\
+                                 to_date, from_date, datetime.date.today(), Json(json_output)])
+    
+    return rfm_json_table                             
